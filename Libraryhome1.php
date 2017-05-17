a97642e7432da01c1d5adf126a69e36766340416
Shikha Nepal <myworldshikha5@gmail.com>
2017-05-16 20:07:38 -0700

    Css and php file

---
+++ Libraryhome1.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+	<head>
+		<link rel = "stylesheet" type = "text/css" href="Libraryhome1.css" />
+		
+	</head>
+	<body>
+		<div class="container"><!--everything on the page-->
+			<div class="row">
+				<header id= "nav">
+					<div id= "nav_wrapper">
+					<ul>
+						<li><a href="Welcome.php">Home</a></li> 
+						<li><a href="news.php">History</a></li>
+						<li><a href="setting.php">Setting</a></li>
+						<li><a href="about.php">About</a></li>
+					</ul>
+					</div>
+				</header>
+			</div> <!--closing row 1-->
+
+			<!--<div class="row">
+				<div class="hero-image">
+				</div>
+			</div>--><!--closing row 2-->
+
+		<div class= "flexbox"> <!--Container-->
+
+			<div class="row">	<!--First row-->
+				<div class="row3">
+					<div class="three">
+						<div class= "imagebox">
+  							<img src="image/wikipedia4.jpg" alt="1.picture" width= 100% height= auto/>
+  							<div class ="textbox">
+  								<h1> Wikipedia </h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="three">
+						<div class= "imagebox">
+						<img src="image/simulation.jpg" alt="3.picture" width= 100% height= 100%/>
+  							<div class= "textbox">
+  				 				<h1> Simulation </h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="three middle2">
+						<div class= "imagebox">
+						<img src="image/elibrary.jpeg" alt="3.picture" width= 100% height= 100%/>
+  							<div class= "textbox">
+  				 				<h1> Epustakalaya </h1>
+  				 				<div class= "logo">
+  				 					<img src= "image/epustakalaya.jpg" alt="logo"/>
+  				 				</div>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="three">
+						<div class= "imagebox">
+  							<img src="image/library.jpg" alt="Library Management System" width= 100% height= 100%/>
+  							<div class= "textbox">
+  				 				<h1> Library Management System </h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+				</div>
+			</div><!--closing row 3-->
+
+
+			<div class="row"> <!--Second row-->
+				<div class="row4">
+					<div class="three">
+						<div class="imagebox">
+  							<img src="image/portfolio1.jpg" alt="4.picture" width= 100% height= 100%/>
+  							<div class= "textbox">
+  						 		<h1> Portfolio </h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="six middle">
+							<hr>
+							<h1> Library In A Box </h1>
+							<br> <br>
+							<hr>
+						<div id= "mainDiv">
+							<div id="boxDiv">
+								<div id= "front">
+									<img src= "image/bookcamera.jpg" width=100% height= 100%/>
+								</div>
+								<div id= "back">
+									<img src= "image/effiltower.jpg" width=100% height= 100%/>
+								</div>
+								<div id= "left">
+									<img src= "image/headphone.jpg" width=100% height= 100%/> 
+								</div>
+								<div id= "right">
+									<img src= "image/laptopbook.jpg" width=100% height= 100%/> 
+								</div>
+								<div id= "top"> <br>  </div>
+								<div id= "bottom"> <br>  </div>
+							</div>
+						</div>
+
+					</div><!--closing column 6-->
+
+					<div class="three">
+						<div class="imagebox">
+  							<img src="image/ibook_blog.jpg" alt="4.picture" width= 100% height= 100%/>
+  							<div class= "textbox">
+  						 		<h1> Elearning </h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+				</div>
+			</div><!--closing row 4-->
+
+			<div class="row"> <!-- Thid row-->
+				<div class="row5">
+					<div class="three">
+						<div class="imagebox">
+  							<img src="image/gutenberg1.png" alt="8.picture" width= 100% height= 100%/>
+  							<div class= "textbox">
+  				 				<h1> Gutenberg</h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="three">
+						<div class= "imagebox">
+  							<img src="image/digitalbooks.jpeg" alt="11.picture" width= 100% height= 100%/>
+  							<div class="textbox">
+  								<h1>School Management System</h1>
+  							</div>
+  						</div>
+					</div><!--closing column 3-->
+
+					<div class="three">
+						<div class= "imagebox">
+  						<img src="image/robot (1).jpg" alt="6.picture" width= 100% height= 100%/>
+  						<div class="textbox">
+  							<h1>Science Fiction </h1>
+  						</div>
+  					</div>
+					</div><!--closing column 3-->
+
+					<div class="three">
+						<div class="imagebox right">
+  						<img src="image/blog.png" alt="10.picture" width= 100% height= 100%/>
+  						<div class="textbox">
+  							<h1>Blog</h1>
+  						</div>
+					</div><!--closing column 3-->
+				</div>
+			</div><!--closing row 5-->
+		</div>
+		</div>
+
+		
+			<div class="row">
+				<div class="foot">
+		
+				</div><!--closing row 8-->
+			</div>
+
+
+	</div><!--closing container div-->
+	</body>
+</html>
